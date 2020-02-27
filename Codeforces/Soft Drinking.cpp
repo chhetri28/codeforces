2e3cc50a4ee2d76c03dd42b826179669ed7858db
chhetri28 <chettrisourav28@gmail.com>
2020-02-27 19:30:25 +0530

    easy

---
+++ Codeforces/Soft Drinking.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,k,l,c,d,p,nl,np;
+    cin>>n>>k>>l>>c>>d>>p>>nl>>np;
+    cout<<min(min(k*l/nl,c*d),p/np)/n<<endl;
+}
+
+/*
+log yeha k kale bichoou
+pal pal dasne walw bichoou
+tha malum ki dass lenge
+phir v shook se pale bichoou
+*/
