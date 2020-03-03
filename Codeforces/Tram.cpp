82738141719e0a97aebb5e988828aa796b1e3c1f
chhetri28 <chettrisourav28@gmail.com>
2020-03-04 03:10:50 +0530

    easy

---
+++ Codeforces/Tram.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int a,b,c(0),m(0);
+    while(n--){
+        cin>>a>>b;
+        c-=a;
+        c+=b;
+        if(c>m){
+        m=c;
+        }
+    }
+    cout<<m<<endl;
+}
