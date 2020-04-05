408709b865cc7392844264d7f2da8ad94e7c8036
chhetri28 <chettrisourav28@gmail.com>
2020-04-06 00:42:11 +0530

    easy

--- Codeforces/Lineland Mail.cpp
+++ Codeforces/Lineland Mail.cpp
@@ -1,5 +1,5 @@
 #include<bits/stdc++.h>
-using namespace stdl;
+using namespace std;
 int main(){
     int n;
     cin>>n;
@@ -7,5 +7,26 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>a[i];
     }
-    
+    int mini,maxi;
+    for(int i=0;i<n;i++){
+        if(i==0){
+            mini=a[1]-a[0];
+            maxi=a[n-1]-a[0];
+        }
+        else if(i==(n-1)){
+            mini=a[n-1]-a[n-2];
+            maxi=a[n-1]-a[0];
+        }
+        else{
+            mini=a[i]-a[i-1];
+            if(a[i+1]-a[i]<mini){
+                mini=a[i+1]-a[i];
+            }
+            maxi=a[n-1]-a[i];
+            if(a[i]-a[0]>maxi){
+                maxi=a[i]-a[0];
+            }
+        }
+    cout<<mini<<" "<<maxi<<endl;
+    }
 }
