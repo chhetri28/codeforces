003ca1fd551e2ebe36114622dd9aca1be56cbc74
chhetri28 <chettrisourav28@gmail.com>
2020-07-11 19:57:21 +0530

    Simple

---
+++ Codeforces/Yet Another Tetris Problem.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef vector <int> vi;
+typedef pair< int ,int > pii;
+#define endl "\n"
+#define sd(val) scanf("%d",&val)
+#define ss(val) scanf("%s",&val)
+#define sl(val) scanf("%lld",&val)
+#define debug(val) printf("check%d\n",val)
+#define all(v) v.begin(),v.end()
+#define PB push_back
+#define MP make_pair
+#define FF first
+#define SS second
+#define ll long long
+#define MOD 1000000007
+#define clr(val) memset(val,0,sizeof(val))
+#define what_is(x) cerr << #x << " is " << x << endl; 
+#define OJ \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+#define FIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+
+int main() 
+{
+    OJ;
+    FIO;
+ int t;
+ cin>>t;
+ while(t--){
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    int cnt[2]={};
+    for(int i=0;i<n;i++){
+        cnt[a[i]%2]++;
+    }
+    if(cnt[0] and cnt[1]){
+        cout<<"NO"<<endl;
+    }
+    else{
+        cout<<"YES"<<endl;
+    }
+ }
+return 0;   
+}
