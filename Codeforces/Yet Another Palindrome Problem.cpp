fe03cf75744b1994a96bbfb5e6df937cf19b260d
chhetri28 <chettrisourav28@gmail.com>
2020-07-13 11:13:46 +0530

    easy

---
+++ Codeforces/Yet Another Palindrome Problem.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef vector <int> vi;
+typedef pair< int ,int > pii;
+#define endl "\n"
+#define sd(val) scanf("%d",&val)
+#define ss(val) scanf("%s",&val)
+#define sl(val) scanf("%lld",&val)
+#define debug(val) printf("check%d\n",val)
+#define all(v) v.begin(),v.end()
+#define PB push_back
+#define MP make_pair
+#define FF first
+#define SS second
+#define ll long long
+#define MOD 1000000007
+#define clr(val) memset(val,0,sizeof(val))
+#define what_is(x) cerr << #x << " is " << x << endl; 
+#define OJ \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+#define FIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int main(){
+    OJ;
+    FIO;
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int a[n];
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        string ans="NO";
+        // freq>=3
+        map<int,vi>mp;
+        for(int i=0;i<n;i++){
+            mp[a[i]].PB(i);
+        }
+        for(auto el:mp){
+            if(el.SS.size()>=3){
+                ans="YES";
+                break;
+            }
+        }
+        //non-adjacent equal elements
+        for(auto el:mp){
+            if(el.SS.size()>=2 and el.SS[0]!=el.SS[1]-1){
+                ans="YES";
+                break;
+            }
+        }
+        cout<<ans<<endl;
+    }
+}
