94daf281a5611534d79a852217ccdeaa0e031115
chhetri28 <chettrisourav28@gmail.com>
2020-07-25 21:37:29 +0530

    easy

--- Codeforces/Is it rated.cpp
+++ Codeforces/Is it rated.cpp
@@ -1,36 +1,61 @@
 #include<bits/stdc++.h>
-#define ll long long 
 using namespace std;
+
+typedef vector <int> vi;
+typedef pair< int ,int > pii;
+#define endl "\n"
+#define sd(val) scanf("%d",&val)
+#define ss(val) scanf("%s",&val)
+#define sl(val) scanf("%lld",&val)
+#define debug(val) printf("check%d\n",val)
+#define all(v) v.begin(),v.end()
+#define PB push_back
+#define MP make_pair
+#define FF first
+#define SS second
+#define ll long long
+#define MOD 1000000007
+#define clr(val) memset(val,0,sizeof(val))
+#define what_is(x) cerr << #x << " is " << x << endl; 
+#define OJ \
+	freopen("input.txt", "r", stdin); \
+	freopen("output.txt", "w", stdout);
+#define FIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int a[1001],b[1001],c[1001];
+int n;
+int x,y;
 int main(){
-   int n;
-   cin>>n;
-   int a1[n],a2[n],a3[n];
-   for(int i=0;i<n;i++){
-       int a,b;
-       cin>>a>>b;
-       a1[i]=a;
-       a2[i]=b;
-       a3[i]=b;
-   }
-   int c1(0),c2(0);
-   for(int i=0;i<n;i++){
-       if(a1[i]==a2[i]){
-           c1++;
-       }
-   }
-   sort(a2,a2+n);
-   for(int i=n-1,j=0;i>=0;i--,j++){
-       if(a2[i]==a3[i]){
-           c2++;
-       }
-   }
-   if(c1!=n){
-       cout<<"rated";
-   }
-   else if(c1==n and c2==n){
-       cout<<"maybe";
-   }
-   else{
-       cout<<"unrated";
-   }
+	//OJ;
+	FIO;
+	cin>>n;
+	for(int i=0;i<n;i++){
+		cin>>x>>y;
+		a[i]=x;
+		b[i]=y;
+		c[i]=y;
+	}
+	int c1(0),c2(0);
+	for(int i=0;i<n;i++){
+		if(a[i]==b[i]){
+			c1++;
+		}
+	}
+	sort(b,b+n);
+	int j=0;
+	for(int i=n-1;i>=0;i--)
+	{
+		if(b[i]==c[j])
+		 ++c2;
+		 j++;
+
+	}
+	if(c1!=n){
+		cout<<"rated\n";
+	}
+	else if(c1==n and c2==n){
+		cout<<"maybe\n";
+	}
+	else{
+		cout<<"unrated\n";
+	}
 }
--- Codeforces/Panoramix's Prediction.cpp
+++ Codeforces/Panoramix's Prediction.cpp
@@ -1,32 +1,51 @@
 #include<bits/stdc++.h>
 using namespace std;
-bool isprime(int n){
-    if(n==1) return false;
-    if(n==2) return true;
-    if(n%2==0) return false;
-    for (size_t i = 3; i < n; i++)
-    {
-        if(n%i==0){
-            return false;
-        }
-    }
-    return true;
-}
+
+typedef vector <int> vi;
+typedef pair< int ,int > pii;
+#define endl "\n"
+#define sd(val) scanf("%d",&val)
+#define ss(val) scanf("%s",&val)
+#define sl(val) scanf("%lld",&val)
+#define debug(val) printf("check%d\n",val)
+#define all(v) v.begin(),v.end()
+#define PB push_back
+#define MP make_pair
+#define FF first
+#define SS second
+#define ll long long
+#define MOD 1000000007
+#define clr(val) memset(val,0,sizeof(val))
+#define what_is(x) cerr << #x << " is " << x << endl; 
+#define OJ \
+	freopen("input.txt", "r", stdin); \
+	freopen("output.txt", "w", stdout);
+#define FIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 int main(){
-    int n,m;
-    cin>>n>>m;
-    vector<int>a;
-    for(int i=n+1;i<=m;i++){
-        if(isprime(i)){
-            if(i==m){
-                cout<<"YES"<<endl;
-                break;
-            }
-            else{
-                cout<<"NO\n";
-                break;
-            }
-        }
-    }
-    cout<<"NO"<<endl;
+	OJ;
+	FIO;
+	int n,m;
+	cin>>n>>m;
+	vector<int>prime;
+	for(int i=2;i<=60;i++){
+		int c(0);
+		for(int j=2;j<=i/2;j++){
+			if(i%j==0){
+				c++;
+				break;
+			}
+		}
+		if(c==0 and i!=1){
+			prime.PB(i);
+		}
+	}
+	vector<int>::iterator it1,it2;
+	it1=find(prime.begin(),prime.end(),n);
+	it2=find(prime.begin(),prime.end(),m);
+	if(it2-it1==1){
+		cout<<"YES\n";
+	}
+	else{
+		cout<<"NO\n";
+	}
 }
